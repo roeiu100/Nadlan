9128e93e085dae7c31820cae1369927a5bf34bf1
roeiu100 <roeiu100@gmail.com>
2023-05-03 15:54:55 +0300

    works

--- src/pages/info/components/Map.jsx
+++ src/pages/info/components/Map.jsx
@@ -2,7 +2,7 @@ import React, { useMemo } from 'react';
 import { GoogleMap, useLoadScript, MarkerF } from '@react-google-maps/api';
 
 const Map = ({lat,lng}) => {
-  const { isLoaded } = useLoadScript({ googleMapsApiKey: '' });
+  const { isLoaded } = useLoadScript({ googleMapsApiKey: 'AIzaSyCAGM2AntGf_ChNk3lm2XvrA1gFe4nq9SU' });
 
   const center =useMemo(()=>({ lat: lat, lng: lng }),[lat,lng]) ;
 
